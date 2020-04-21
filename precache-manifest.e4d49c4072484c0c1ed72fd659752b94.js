20efb16807d3aae59388384a7ee5e63d33ef15e8
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-04-20 21:28:30 -0700

    Updates

---
+++ precache-manifest.e4d49c4072484c0c1ed72fd659752b94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b05c7af36d39eb1fd7f8863931dcb01",
+    "url": "/Dog-Breed-Quiz/index.html"
+  },
+  {
+    "revision": "e076f923cdef7e8857ee",
+    "url": "/Dog-Breed-Quiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6358e8e1d6200795720",
+    "url": "/Dog-Breed-Quiz/static/js/2.87218dae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dog-Breed-Quiz/static/js/2.87218dae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e076f923cdef7e8857ee",
+    "url": "/Dog-Breed-Quiz/static/js/main.3d2474ef.chunk.js"
+  },
+  {
+    "revision": "e4c15e06dab0ee0a7d2a",
+    "url": "/Dog-Breed-Quiz/static/js/runtime-main.5ce3ac0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dog-Breed-Quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
